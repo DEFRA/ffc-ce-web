d3317daa14d711dc13de815a93945ffa43b94e00
David Bingham <david.bingham@defra.gov.uk>
2020-01-20 10:10:02 +0000

    Correct StandardJS lint issues

--- server/public/static/javascript/parcels.js
+++ server/public/static/javascript/parcels.js
@@ -8,8 +8,8 @@ ParcelsPage.prototype.init = function () {
     return
   }
 
-  $filter = $module.querySelector('#parcel-ref-filter')
-  $parcels = $module.getElementsByClassName('govuk-radios__item')
+  const $filter = $module.querySelector('#parcel-ref-filter')
+  const $parcels = $module.querySelectorAll('.govuk-radios__item')
 
   $filter.addEventListener('input', event => filterParcels(event.target, $parcels))
 }
@@ -17,20 +17,17 @@ ParcelsPage.prototype.init = function () {
 function filterParcels ($filter, $parcels) {
   const filterIsEmpty = $filter.value.length === 0
 
-  for (let parcelIndex in $parcels) {
-    if ($parcels.hasOwnProperty(parcelIndex)) {
-      const $parcel = $parcels[parcelIndex]
-      const parcelRef = $parcel.querySelector('input[name=parcelRef]').value
+  $parcels.forEach($parcel => {
+    const parcelRef = $parcel.querySelector('input[name=parcelRef]').value
 
-      const parcelMatchesInput = new RegExp($filter.value).test(parcelRef)
+    const parcelMatchesInput = new RegExp($filter.value).test(parcelRef)
 
-      if (filterIsEmpty || parcelMatchesInput) {
-        $parcel.classList.remove('hidden')
-      } else {
-        $parcel.classList.add('hidden')
-      }
+    if (filterIsEmpty || parcelMatchesInput) {
+      $parcel.classList.remove('hidden')
+    } else {
+      $parcel.classList.add('hidden')
     }
-  }
+  })
 }
 
 new ParcelsPage(document).init()
