c399b613d66c95da66ffa3aca935e20c3b9aac0b
Paul Andrews <paul.andrews@defra.gov.uk>
2020-01-23 12:51:29 +0000

    Remove console.logs

--- server/services/payments-service.js
+++ server/services/payments-service.js
@@ -4,15 +4,14 @@ const wreck = require('@hapi/wreck').defaults({
 })
 
 async function calculatePayment (parcelRef, actionId, quantity) {
-  const payloadData = JSON.stringify({ quantity })
-  console.log('In updated calculatePayment')
-  console.log(payloadData)
+  const payloadData = JSON.stringify({ actionData: { quantity } })
   const result = await wreck.post(
     `${config.paymentUrl}/parcels/${parcelRef}/actions/${actionId}/payment-calculation`,
-    { json: true, payload: payloadData }
+    {
+      json: true,
+      payload: payloadData
+    }
   )
-
-  console.log(result.payload)
 
   return result.payload
 }
