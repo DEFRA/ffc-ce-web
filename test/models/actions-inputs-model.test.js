1edb5bac946ef03eaa8db84aa3b68f59b87781b5
Mark Harrop <markharrop@yahoo.com>
2020-01-28 16:10:25 +0000

    Provide targeted message for actions (#20)
    * Provide targeted message for actions
    * add action specific hint and title
    
    https://eaflood.atlassian.net/browse/FCEP-104

--- test/models/actions-inputs-model.test.js
+++ test/models/actions-inputs-model.test.js
@@ -33,4 +33,16 @@ describe('actions inputs model', () => {
     const model = actionsInputsModel('', '')
     expect(model.label.classes).toBe('govuk-label--xl')
   })
+
+  test('title and hint refers to fencing for Action ID FG1', () => {
+    const model = actionsInputsModel('', 'FG1')
+    expect(model.label.text).toBe('Enter a fence length in metres')
+    expect(model.hint.text).toBe('How long is the fence you want to claim on in metres?')
+  })
+
+  test('title and hintrefers to acres for Action ID SW6', () => {
+    const model = actionsInputsModel('', 'SW6')
+    expect(model.label.text).toBe('Enter the area in hectares')
+    expect(model.hint.text).toBe('What is the area you wish to claim on in hectares?')
+  })
 })
