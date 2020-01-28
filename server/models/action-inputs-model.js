eef1ebb4c86afb9bd4c9d4801e11335cc6f9cffd
markysoft <mark.harrop@defra.gov.uk>
2020-01-28 15:54:58 +0000

    add action specific hint and title

--- server/models/action-inputs-model.js
+++ server/models/action-inputs-model.js
@@ -1,12 +1,20 @@
+function getTitle (actionId) {
+  return actionId === 'FG1' ? 'Enter a fence length in metres' : 'Enter the area in hectares'
+}
+
+function getHint (actionId) {
+  return actionId === 'FG1' ? 'How long is the fence you want to claim on in metres?' : 'What is the area you wish to claim on in hectares?'
+}
+
 function actionsInputsModel (parcelRef, actionId, error) {
   const model = {
     hint: {
-      text: 'How long is the fence you want to claim on in metres?'
+      text: getHint(actionId)
     },
     label: {
       classes: 'govuk-label--xl',
       isPageHeading: true,
-      text: 'Enter a fence length in metres'
+      text: getTitle(actionId)
     },
     classes: 'govuk-input--width-10',
     id: 'actionInput',
