ad38d21ab1b8b9a55d81794bb71c632da30ecc81
markysoft <mark.harrop@defra.gov.uk>
2020-01-28 15:24:23 +0000

    display different title and hint depending on action

--- server/models/action-inputs-model.js
+++ server/models/action-inputs-model.js
@@ -11,23 +11,15 @@ function boundsValid (action) {
   return action && action.inputs && action.inputs[0] && action.inputs[0].upperbound && action.inputs[0].lowerbound
 }
 
-function getBounds (action) {
-  return boundsValid(action)
-    ? action.inputs.find(input => input.upperbound !== undefined && input.lowerbound !== undefined)
-    : undefined
-}
-
-function boundsMessage (bounds) {
-  return `Please choose a value between ${bounds.lowerbound} and ${bounds.upperbound}.`
+function boundsMessage (action) {
+  return `Please choose a value between ${action.inputs[0].lowerbound} and ${action.inputs[0].upperbound}`
 }
 
 function getBoundsHint (action) {
-  const bounds = getBounds(action)
-  return bounds ? boundsMessage(bounds) : ''
+  return boundsValid(action) ? boundsMessage(action) : ''
 }
 
 function actionsInputsModel (parcelRef, action, error, parcels) {
-  console.log({ action })
   const model = {
     hint: {
       text: `${getHint(action.id)} ${getBoundsHint(action)}`
--- test/models/actions-inputs-model.test.js
+++ test/models/actions-inputs-model.test.js
@@ -4,34 +4,34 @@ const allParcels = [{ ref: 'AAA', description: 'Test parcel', totalPerimeter: 10
 describe('actions inputs model', () => {
   test('if error provided, should populate errorMessage', () => {
     const error = 'sample error message'
-    const model = actionsInputsModel('', '', error).model
+    const model = actionsInputsModel('', {}, error).model
     expect(model.errorMessage).toEqual(
       expect.objectContaining({ text: error })
     )
   })
 
   test('if error isn\'t provided, errorMessage should be omitted', () => {
-    const model = actionsInputsModel('', '').model
+    const model = actionsInputsModel('', {}).model
     expect(model.errorMessage).toBeUndefined()
   })
 
   test('label class set to govuk-label--xl', () => {
-    const model = actionsInputsModel('', '').model
+    const model = actionsInputsModel('', {}).model
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
   test('label isPageHeading is true', () => {
-    const model = actionsInputsModel('', '').model
+    const model = actionsInputsModel('', {}).model
     expect(model.label.isPageHeading).toBeTruthy()
   })
 
   test('id is actionInput', () => {
-    const model = actionsInputsModel('', '').model
+    const model = actionsInputsModel('', {}).model
     expect(model.id).toBe('actionInput')
   })
 
   test('name is actionInput', () => {
-    const model = actionsInputsModel('', '').model
+    const model = actionsInputsModel('', {}).model
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
