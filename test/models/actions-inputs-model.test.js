d3f66aa832542d515d3740bb0765cd425c6a5eb3
markysoft <mark.harrop@defra.gov.uk>
2020-01-28 15:24:23 +0000

    display different title and hint depending on action

--- test/models/actions-inputs-model.test.js
+++ test/models/actions-inputs-model.test.js
@@ -3,34 +3,34 @@ const actionsInputsModel = require('../../server/models/action-inputs-model')
 describe('actions inputs model', () => {
   test('if error provided, should populate errorMessage', () => {
     const error = 'sample error message'
-    const model = actionsInputsModel('', '', error)
+    const model = actionsInputsModel('', {}, error)
     expect(model.errorMessage).toEqual(
       expect.objectContaining({ text: error })
     )
   })
 
   test('if error isn\'t provided, errorMessage should be omitted', () => {
-    const model = actionsInputsModel('', '')
+    const model = actionsInputsModel('', {})
     expect(model.errorMessage).toBeUndefined()
   })
 
   test('label class set to govuk-label--xl', () => {
-    const model = actionsInputsModel('', '')
+    const model = actionsInputsModel('', {})
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
   test('label isPageHeading is true', () => {
-    const model = actionsInputsModel('', '')
+    const model = actionsInputsModel('', {})
     expect(model.label.isPageHeading).toBeTruthy()
   })
 
   test('id is actionInput', () => {
-    const model = actionsInputsModel('', '')
+    const model = actionsInputsModel('', {})
     expect(model.id).toBe('actionInput')
   })
 
   test('name is actionInput', () => {
-    const model = actionsInputsModel('', '')
+    const model = actionsInputsModel('', {})
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
