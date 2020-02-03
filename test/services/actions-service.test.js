f7c3f3af6fabef3ede634bab3d0f16c9551c3472
Phil Benson <phil.benson@defra.gov.uk>
2020-02-03 11:33:20 +0000

    Simplified mocking in actions service tests

--- test/services/actions-service.test.js
+++ test/services/actions-service.test.js
@@ -1,50 +1,34 @@
-let actionsService
+const wreck = require('@hapi/wreck')
 jest.mock('@hapi/wreck')
-
-let payload
-const parcelRef = 'AB12345678'
-
-function stubWreckCall () {
-  const wreck = require('@hapi/wreck')
-  wreck.defaults = () => {
-    return {
-      get: (ref) => {
-        return Promise.resolve({ payload })
-      }
-    }
-  }
-}
+wreck.defaults.mockImplementation(() => wreck)
+const actionsService = require('../../server/services/actions-service')
 
 describe('actionService', () => {
-  beforeAll(() => {
-    // Test follows the structure of parcels-service.test.js, which says:
-    // "I tried to stub the call before each, but only the return setup in the first test
-    // was returned, despite trying a combination of clear/reset mocks so I resorted to
-    // setting the payload to a local variable that can be changed before each test
-    // https://github.com/facebook/jest/issues/7136
-    // issue was fixed in November. but not perhaps not made it into the lastest version,
-    // though there are still requests on the issue above to reopen the bug"
-    stubWreckCall()
-    actionsService = require('../../server/services/actions-service')
+  describe('getActions', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('get actions return JSON', async () => {
+      const mockActions = { payload: { actions: [{ id: '1', description: 'an action' }] } }
+      wreck.get.mockResolvedValue(mockActions)
+      const result = await actionsService.getActions('AB12345678')
+      expect(result).toBeDefined()
+      expect(result.length).toEqual(1)
+      expect(result[0]).toEqual(mockActions.payload.actions[0])
+    })
+
+    test('get actions returns empty array for empty payload', async () => {
+      wreck.get.mockResolvedValue({})
+      const result = await actionsService.getActions('AB12345678')
+      expect(result).toBeDefined()
+      expect(result.length).toEqual(0)
+    })
   })
 
-  test('get actions return JSON', async () => {
-    const mockActions = { actions: [{ id: '1', description: 'an action' }] }
-    payload = mockActions
-    const result = await actionsService.getActions(parcelRef)
-    expect(result).toBeDefined()
-    expect(result.length).toEqual(1)
-    expect(result[0]).toEqual(mockActions.actions[0])
-  })
-
-  test('get actions returns empty array for empty payload', async () => {
-    payload = undefined
-    const result = await actionsService.getActions(parcelRef)
-    expect(result).toBeDefined()
-    expect(result.length).toEqual(0)
-  })
-
-  afterAll(() => {
-    jest.unmock('@hapi/wreck')
+  describe('getActionWithInput', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
   })
 })
