a108f736e4f3d9822c6d2efcbb51ab41375581e3
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2020-02-04 09:28:37 +0000

    No longer need to populate the parcel ref

--- test/models/actions-inputs-model.test.js
+++ test/models/actions-inputs-model.test.js
@@ -1,5 +1,5 @@
 const actionsInputsModel = require('../../server/models/action-inputs-model')
-const allParcels = [{ ref: 'SD74445738', description: 'Test parcel', totalPerimeter: 10, totalArea: 1 }]
+const allParcels = [{ ref: '', description: 'Test parcel', totalPerimeter: 10, totalArea: 1 }]
 
 describe('actions inputs model', () => {
   const actions = {
@@ -22,34 +22,34 @@ describe('actions inputs model', () => {
   }
   test('if error provided, should populate errorMessage', () => {
     const error = 'sample error message'
-    const { model } = actionsInputsModel('SD74445738', actions.fg6, error)
+    const { model } = actionsInputsModel('', actions.fg6, error)
     expect(model.errorMessage).toEqual(
       expect.objectContaining({ text: error })
     )
   })
 
   test('if error isn\'t provided, errorMessage should be omitted', () => {
-    const { model } = actionsInputsModel('SD74445738', actions.fg6)
+    const { model } = actionsInputsModel('', actions.fg6)
     expect(model.errorMessage).toBeUndefined()
   })
 
   test('label class set to govuk-label--xl', () => {
-    const { model } = actionsInputsModel('SD74445738', actions.fg6)
+    const { model } = actionsInputsModel('', actions.fg6)
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
   test('label isPageHeading is true', () => {
-    const { model } = actionsInputsModel('SD74445738', actions.fg6)
+    const { model } = actionsInputsModel('', actions.fg6)
     expect(model.label.isPageHeading).toBeTruthy()
   })
 
   test('id is actionInput', () => {
-    const { model } = actionsInputsModel('SD74445738', actions.fg6)
+    const { model } = actionsInputsModel('', actions.fg6)
     expect(model.id).toBe('actionInput')
   })
 
   test('name is actionInput', () => {
-    const { model } = actionsInputsModel('SD74445738', actions.fg6)
+    const { model } = actionsInputsModel('', actions.fg6)
     expect(model.label.classes).toBe('govuk-label--xl')
   })
 
@@ -68,7 +68,7 @@ describe('actions inputs model', () => {
   })
 
   test('Parcel data is included in the model when a parcel is included', () => {
-    const parcelModel = actionsInputsModel('SD74445738', actions.fg6, '', allParcels).parcelModel
+    const { parcelModel } = actionsInputsModel('', actions.fg6, '', allParcels)
     expect(parcelModel.caption).toBe('Parcel Information')
   })
 
